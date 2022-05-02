283981b528bd7c5fea4e322b6e8fb72c15f2eec8
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-05-02 11:35:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+
+// ARRAY FOR SERVICES AND PRICES
+const serviceItems = ["Car Wash", "Mow Lawn", "Pull Weeds"]
+const Item1 = "10"
+const Item2 = "20"
+const Item3 = "30"
+
+
+// LINKS ITEM LIST TO SERVICE BUTTONS
+const itemCar = document.querySelector(".item1") 
+const itemLawn = document.querySelector(".item2")
+const itemWeeds = document.querySelector(".item3")
+
+// SERVICES
+function renderItems(){
+    for (let i = 0; i <serviceItems.length; i++){
+        console.log(serviceItems[i])
+    }
+}
+
+
+// CAR WASH BUTTON 
+const carWash = document.querySelector("#car-wash") 
+const carPrice = document.querySelector(".price1")
+carWash.addEventListener("click", function(){
+    itemCar.textContent = serviceItems[0]
+    carPrice.textContent =  '$' + Item1
+})
+
+
+// MOW LAWN BUTTON
+const mowLwan = document.querySelector("#mow-lawn")
+const lawnPrice = document.querySelector(".price2")
+mowLwan.addEventListener("click", function(){
+    itemLawn.textContent = serviceItems[1]
+    lawnPrice.textContent = '$' + Item2
+})
+
+
+// WEEDS
+const pullWeeds = document.querySelector("#pull-weeds")
+const weedPrice = document.querySelector(".price3")
+pullWeeds.addEventListener("click", function(){
+    itemWeeds.textContent = serviceItems[2]
+    weedPrice.textContent = '$' + Item3
+    priceTotal()
+})
+
+
+// RENDER TOTAL PRICE
+function priceTotal(){
+    const totalPrice = document.querySelector("#total-amount")
+    const allCharges = parseInt(Item1) + parseInt(Item2)  + parseInt(Item3) 
+    totalPrice.textContent =  "$" + allCharges
+}
+
+
+
+
+//INVOICE BUTTON
+
+
+
